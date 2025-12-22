393fa1324425f17f839ba68e979369be56fca281
ratplier <133662217+ratplier@users.noreply.github.com>
2025-12-21 19:55:11 -0600

    Add expression nodes

---
+++ crates/ast/src/expression.rs
@@ -0,0 +1,36 @@
+use crate::operators::{BinaryOp, UnaryOp};
+use stock_source::{Span, Symbol};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ExprId(pub u32);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum LiteralKind {
+    Integer,
+    Float,
+}
+
+#[derive(Debug, Clone)]
+pub enum ExprKind {
+    Literal {
+        kind: LiteralKind,
+        value: Symbol, 
+    },
+
+    Binary {
+        op: BinaryOp,
+        lhs: ExprId,
+        rhs: ExprId,
+    },
+
+    Unary {
+        op: UnaryOp,
+        rhs: ExprId,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub struct Expr {
+    pub kind: ExprKind,
+    pub span: Span,
+}
--- crates/ast/src/lib.rs
+++ crates/ast/src/lib.rs
@@ -1,5 +1,8 @@
 mod operators;
+mod expression;
+mod arena;
 
 pub use crate::{
-    operators::{BinaryOp, UnaryOp}
+    operators::{BinaryOp, UnaryOp},
+    expression::{Expr, ExprId, ExprKind, LiteralKind},
 };
